12bc400a0279d708a366dbda0d43f96f03b43728
GsCabral <cabralgonzalo10@gmail.com>
2025-01-09 10:29:53 -0300

    cambio puerto

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const server = require ("./src/app.js");
 const {conn}=require('./src/db.js');
 
+
+const PORT= process.env.PORT || 3006;
+
 conn.sync({force:false}).then(()=>{
-    server.listen(3006,()=>{
+    server.listen(PORT,()=>{
         console.log("listen at 3006")
     })
 })
